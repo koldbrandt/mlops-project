0486c9242474df0faa86237e9cd78ca0a5200bd2
Marcus Brandt <mailtilmb@gmail.com>
2022-01-04 10:57:12 +0100

    changed structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Project created in the DTU course 02476',
+    author='Marcus Brandt',
+    license='MIT',
+)
